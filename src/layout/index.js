99a4e675186e7bedb81d4888ae7b8fb543dd0177
Laye <tlayefaah@gmail.com>
2023-09-05 15:38:25 +0100

    added favicon

--- src/layout/index.js
+++ src/layout/index.js
@@ -11,7 +11,27 @@ export default function Layout({ title, children }) {
         <title>{title ? title + " | Stustle" : " Stustle"}</title>
         <meta name="description" content="Service Rendering Website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/stustle_logo.svg" />
+        {/* <link rel="icon" href="/stustle_logo.svg" /> */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#e29507" />
+        <meta name="theme-color" content="#e29507" />
       </Head>
       <div className=" max-h-screen h-screen">
         <header className="bg-green300 px-4 bg-white mx-auto h-[6vh]  max-w-xl ">
