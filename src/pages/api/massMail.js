4495f1958f6160e61799d6b58ea6be70bb773eb8
Laye <tlayefaah@gmail.com>
2023-09-06 15:26:53 +0100

    send emails

---
+++ src/pages/api/massMail.js
@@ -0,0 +1,187 @@
+import nodemailer from "nodemailer";
+
+export default async function handler(req, res) {
+  try {
+    if (!req.body) {
+      return res.status(400).json({ error: "No form data provided" });
+    }
+
+    const { email, subject, message } = req.body;
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.zoho.com",
+      port: 465, // Use Zoho's SMTP port
+      secure: true, // Use SSL
+      auth: {
+        user: process.env.USER_EMAIL_ZOHO, // Replace with your Zoho email address
+        pass: process.env.USER_PASS_ZOHO, // Replace with your Zoho email password
+      },
+    });
+
+    const emailContent = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Stustle</title>
+  </head>
+  <body style="font-family: Arial; font-size: 14px">
+    <div
+      style="
+        margin: 0 auto;
+        background-color: #ffffff;
+        max-width: 400px;
+        width: 100%;
+      "
+    >
+      <div style="padding: 20px 0px 0px">
+        <div style="width: 76px; padding: 0 20px">
+          <img
+            src="https://i.postimg.cc/L5yz4XMM/LOGO.png"
+            alt="Stustle"
+            style="width: 100%"
+          />
+        </div>
+        <div
+          style="
+            width: 100%;
+            overflow: hidden;
+            height: 200px;
+            margin-top: 20px;
+            background-image: url('https://i.postimg.cc/R0tCpQT0/Rectangle-177.png');
+            background-repeat: no-repeat;
+            background-position: center;
+            background-size: contain;
+          "
+        >
+          <h1 style="padding: 0 20px; color: #ffffff">
+            Get tasks off your hands
+          </h1>
+          <h6 style="padding: 0 20px; color: #ffffff; width: 40%">
+            On your busy days rely on us. We run so you can relax.
+          </h6>
+        </div>
+        <div
+          style="width: 80%; margin-top: 20px; font-size: 20px; padding: 0 20px"
+        >
+          <p>Hi there,</p>
+        </div>
+        <div style="width: 80%; padding: 0 20px; margin-top: 30px">
+          <p>
+          ${message}
+          </p>
+        </div>
+        <div style="width: 80%; text-align: center; margin: 30px auto 0">
+          <a
+            style="
+              padding: 10px 5px;
+              background-color: #e29507;
+              color: white;
+              border-radius: 5px;
+              text-decoration: none;
+              font-weight: bold;
+              display: inline-block;
+              border: none;
+              cursor: pointer;
+              width: 40%;
+            "
+            href="https://www.stustle.com/"
+            >Find help now</a
+          >
+        </div>
+        <div style="width: 50%; padding: 0 20px; margin-top: 30px">
+          <p>
+            Best regards,
+            <br />
+            Stustle.
+          </p>
+        </div>
+      </div>
+      <div
+        style="
+          max-width: 400px;
+          background-color: #f0f0f0;
+          margin-top: 40px;
+          text-align: center;
+          font-size: 12px;
+          padding: 5px;
+        "
+      >
+        <p style="color: #096dd9; font-size: 10px">
+          For more information, contact our
+          <a
+            href="tel:+2348115237006"
+            style="color: #eb4335; text-decoration: underline"
+            >customer service</a
+          >
+        </p>
+        <div>
+          <a
+            style="display: inline-block; height: 20px; margin-right: 10px"
+            href="https://instagram.com/stustle_official?igshid=NTc4MTIwNjQ2YQ=="
+          >
+            <img
+              style="height: 100%"
+              src="https://i.postimg.cc/P5tJWFqV/skill-icons-instagram.png"
+              alt="insta"
+            />
+          </a>
+          <a
+            style="display: inline-block; height: 20px; margin-right: 10px"
+            href="https://www.facebook.com/profile.php?id=100090583722573&mibextid=9R9pXO"
+          >
+            <img
+              style="height: 100%"
+              src="https://i.postimg.cc/qMHzR7HT/devicon-facebook.png"
+              alt="insta"
+            />
+          </a>
+          <a
+            style="display: inline-block; height: 20px"
+            href="https://twitter.com/stustle?t=07Nuur6s-COLGQX_JIeGlQ&s=09"
+          >
+            <img
+              style="height: 100%"
+              src="https://i.postimg.cc/X7KXy0ny/devicon-twitter.png"
+              alt="insta"
+            />
+          </a>
+          <a
+            style="display: inline-block; height: 20px"
+            href="https://www.linkedin.com/company/stustle/"
+          >
+            <img
+              style="height: 100%"
+              src="https://i.postimg.cc/hGDwtccf/basil-linkedin-solid.pngg"
+              alt="linkedIn"
+            />
+          </a>
+        </div>
+        <div>
+          <p style="color: #2f2f2f; font-size: 10px">&copy; 2023 Stustle</p>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+`;
+
+    const mailOptions = {
+      from: process.env.USER_EMAIL_ZOHO, // Replace with your email address
+      to: email,
+      subject: subject,
+      html: emailContent,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    // Return a success response
+    return res.status(200).json({ message: "Mail Sent Successfully" });
+  } catch (error) {
+    // Handle any errors that may occur
+    console.error(error);
+    return res
+      .status(500)
+      .json({ error: "An error occurred while sending the email." });
+  }
+}
--- src/pages/sendMails.js
+++ src/pages/sendMails.js
@@ -1,28 +1,64 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import { useFormik } from "formik";
-import { contact_message } from "../../lib/validate";
+import { contact, contact_message } from "../../lib/validate";
 import Loader from "src/components/Loader.js";
 import { toast } from "react-toastify";
 import Layout from "@/layout";
+import axios from "axios";
 
 export default function SendMail({ sheetdata }) {
   const [isFormValid, setIsFormValid] = useState(false);
   const [pending, setPending] = useState(false);
   //   const router = useRouter();
 
-  console.log(sheetdata);
+  //   const emails = sheetdata.data.flat();
+  //   console.log(emails);
 
   const formik = useFormik({
     initialValues: {
+      subject: "",
       message: "",
     },
-    validate: contact_message,
+    validate: contact,
     onSubmit: handleSubmit,
   });
 
+  const emails = [
+    "tlayefaah@gmail.com",
+    "stustledev@gmail.com",
+    "damilola.julius@yahoo.com",
+    "janeagadiabiz@gmail.com",
+    "tjagadia24@gmail.com",
+    "omodeefunmi@gmail.com",
+  ];
+
   async function handleSubmit(values) {
-    console.log(values);
+    const { subject, message } = values;
+    try {
+      const promises = emails?.map(async (email) => {
+        // const email = mail;
+        try {
+          const res = await axios.post("/api/massMail", {
+            email,
+            subject,
+            message,
+          });
+
+          //   toast.success(`sent to ${firstName}`);
+          //   formik.setFieldValue('message', '')
+          //   formik.setFieldValue('subject', '')
+
+          console.log(res);
+        } catch (error) {
+          console.log(error);
+        }
+      });
+
+      await Promise.all(promises);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   useEffect(() => {
@@ -42,9 +78,29 @@ export default function SendMail({ sheetdata }) {
       className="fixed z-[1100] w-screen top-0 bottom-0 flex items-center justify-center backdrop-blur-sm bg-black bg-opacity-50 max-w-3xl "
     >
       <div className="bg-white w-80 rounded-lg p-6 dark:bg-[#00000080] dark:backdrop-blur-sm dark:text-white duration-500 relative">
-        <div className="mt-6">
+        <div className="mt-2">
+          <h1 className="font-semibold text-lg mb-4">Send Mass Mail</h1>
           <form onSubmit={formik.handleSubmit}>
-            <div className="flex flex-col items-center  mb-6">
+            <div className="flex flex-col   mb-6">
+              <label className="text-sm mb-2" htmlFor="subject">
+                Subject
+              </label>
+              <input
+                id="subject"
+                name="subject"
+                type="text"
+                placeholder=""
+                maxLength={350}
+                className={`${getInputClassNames("subject")} py-1`}
+                {...formik.getFieldProps("subject")}
+              />
+              {formik.touched.message && formik.errors.message && (
+                <div className="text-error text-sm">
+                  {formik.errors.message}
+                </div>
+              )}
+            </div>
+            <div className="flex flex-col  mb-6">
               <label className="text-sm mb-2" htmlFor="message">
                 Write a message
               </label>
@@ -82,17 +138,6 @@ export default function SendMail({ sheetdata }) {
     </Layout>
   );
 }
-
-// export async function getServerSideProps() {
-//   const req = await fetch("http://localhost:3001/api/sheet");
-//   const res = await req.json();
-
-//   return {
-//     props: {
-//       sheetdata: res.data,
-//     },
-//   };
-// }
 
 export async function getServerSideProps() {
   try {
